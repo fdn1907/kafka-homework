523faa1e1244502581937854ef487c9e7242d2a6
FilatovDN <FilatovDN@digtp.com>
2025-10-12 17:14:08 +0300

    hw11 changes

---
+++ hw-11/src/main/java/ru/otus/KafkaConsumer.java
@@ -0,0 +1,49 @@
+package main.java.ru.otus;
+
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Map;
+
+public class KafkaConsumer {
+
+    public static void consume() {
+
+        Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+
+        Map<String, Object> consumerConfigurationParametersMap = Map.of(
+                ConsumerConfig.GROUP_ID_CONFIG, "cool_group",
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092",
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+                ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed",
+                ConsumerConfig.FETCH_MAX_BYTES_CONFIG, 2000
+        );
+
+
+        org.apache.kafka.clients.consumer.KafkaConsumer<String, String> consumer = new org.apache.kafka.clients.consumer.KafkaConsumer<>(consumerConfigurationParametersMap);
+
+        consumer.subscribe(Arrays.asList("topic1", "topic2"));
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord<String, String> record : records)
+                    log.info("Получено сообщение: {} Key: {} ", record.value(), record.key());
+            }
+        } finally {
+            consumer.close();
+        }
+
+    }
+
+}
---
+++ hw-11/src/main/java/ru/otus/KafkaProducer.java
@@ -0,0 +1,73 @@
+package main.java.ru.otus;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.errors.ProducerFencedException;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class KafkaProducer {
+    public static void main(String[] args) {
+
+        Logger log = LoggerFactory.getLogger(KafkaProducer.class);
+
+
+        Map<String, Object> producerConfigParametersMap = Map.of(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                ProducerConfig.ACKS_CONFIG, "all",
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                ProducerConfig.TRANSACTIONAL_ID_CONFIG, "transactionId"
+        );
+
+        try {
+            try (var kafkaProducer = new org.apache.kafka.clients.producer.KafkaProducer<String, String>(producerConfigParametersMap)) {
+
+                kafkaProducer.initTransactions();
+                kafkaProducer.beginTransaction();
+
+                for (int i = 0; i < 5; i++) {
+
+                    log.info("отправка сообшения id {} в topic1", i);
+                    kafkaProducer.send(new ProducerRecord<>("topic1", "topic1-всем привет" + i));
+                    kafkaProducer.send(new ProducerRecord<>("topic2", "topic2-всем привет" + i));
+
+                }
+
+                kafkaProducer.commitTransaction();
+                log.info("коммитим транзакцию");
+
+            }
+            catch (RuntimeException e){
+                throw new RuntimeException(e);
+            }
+
+            try (var producer2 = new org.apache.kafka.clients.producer.KafkaProducer<String, String>(producerConfigParametersMap)) {
+
+                producer2.initTransactions();
+                producer2.beginTransaction();
+
+                for (int i = 0; i < 2; i++) {
+
+                    log.info("Отсылаем сообщение id {} в топики и далее откатим их", i);
+                    producer2.send(new ProducerRecord<>("topic1", "topic1 message-откатим транзакцию" + i));
+                    producer2.send(new ProducerRecord<>("topic2", "topic2 message-откатим транзакцию" + i));
+                }
+
+
+                producer2.flush();
+
+                producer2.abortTransaction();
+                log.info("commit и далее abort abortTransaction");
+
+            }
+
+        } catch (ProducerFencedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
